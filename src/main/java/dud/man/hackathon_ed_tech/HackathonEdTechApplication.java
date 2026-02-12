eff601f436c37728b296ea672d4d49f7d71ce30e
aranyab0924 <aranya0924@gmail.com>
2026-02-12 10:49:34 -0800

    oh yeah we started spring

---
+++ src/main/java/dud/man/hackathon_ed_tech/HackathonEdTechApplication.java
@@ -0,0 +1,13 @@
+package dud.man.hackathon_ed_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonEdTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonEdTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dud/man/hackathon_ed_tech/HackathonEdTechApplicationTests.java
@@ -0,0 +1,13 @@
+package dud.man.hackathon_ed_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonEdTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
