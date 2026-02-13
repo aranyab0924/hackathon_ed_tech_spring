9a9889f2771b747eda7b46260459859ce74082b4
Tvick22 <142449252+Tvick22@users.noreply.github.com>
2026-02-13 11:16:59 -0800

    OAuth2.0 working with github

--- src/main/java/dud/man/hackathon_ed_tech/HackathonEdTechApplication.java
+++ src/main/java/dud/man/hackathon_ed_tech/HackathonEdTechApplication.java
@@ -1,10 +1,30 @@
 package dud.man.hackathon_ed_tech;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HackathonEdTechApplication {
+
+	@GetMapping("/user")
+		public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+			return Collections.singletonMap("name", principal.getAttribute("name"));
+		}
+
+	@GetMapping("/csrf")
+	public Map<String, String> csrf(CsrfToken token) {
+		return Collections.singletonMap("token", token.getToken());
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackathonEdTechApplication.class, args);
